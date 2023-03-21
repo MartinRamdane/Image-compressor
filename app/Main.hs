79087510f1a580785e2b3bb155464f389e562a7a
Ramdane Martin <martin.ramdane@epitech.eu>
2023-03-21 20:52:14 +0100

    add project with stack

---
+++ app/Main.hs
@@ -0,0 +1,12 @@
+{-
+-- EPITECH PROJECT, 2023
+-- imageCompressor.hs
+-- File description:
+-- imageCompressor
+-}
+
+
+import System.Exit
+
+main :: IO ()
+main = exitWith (ExitFailure 84)
---
+++ src/Lib.hs
@@ -0,0 +1,13 @@
+{-
+-- EPITECH PROJECT, 2023
+-- Lib.hs
+-- File description:
+-- lib
+-}
+
+module Lib
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
